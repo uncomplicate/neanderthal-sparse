9b1d98d4cc7dc5919fe5dcd302917c774c0321ae
Dragan Djuric <dragandj@gmail.com>
2023-02-24 13:26:51 +0100

    Math and RNG extensions for JavaCPP MKL vector engines.

--- src/clojure/uncomplicate/neanderthal/internal/cpp/mkl/factory.clj
+++ src/clojure/uncomplicate/neanderthal/internal/cpp/mkl/factory.clj
@@ -1,8 +1,9 @@
 (ns uncomplicate.neanderthal.internal.cpp.mkl.factory
   (:require [uncomplicate.commons
              [core :refer [with-release let-release info Releaseable release]]
-             [utils :refer [dragan-says-ex generate-seed direct-buffer]]]
+             [utils :refer [dragan-says-ex generate-seed]]]
             [uncomplicate.fluokitten.core :refer [fmap!]]
+            [uncomplicate.clojure-cpp :refer [long-pointer]]
             [uncomplicate.neanderthal
              [core :refer [dim]]
              [math :refer [f=] :as math]
@@ -15,9 +16,11 @@
              [structures :refer :all]
              [lapack :refer :all]
              [blas :refer [float-ptr double-ptr vector-imax vector-imin]]]
-            [uncomplicate.neanderthal.internal.cpp.mkl.core :refer [malloc!]])
+            [uncomplicate.neanderthal.internal.cpp.mkl.core :refer [malloc!]]
+            [uncomplicate.neanderthal.internal.host.mkl
+             :refer [sigmoid-over-tanh vector-ramp vector-relu vector-elu]])
   (:import [uncomplicate.neanderthal.internal.api DataAccessor Block Vector]
-           [org.bytedeco.mkl.global mkl_rt]))
+           [org.bytedeco.mkl.global mkl_rt mkl_rt$VSLStreamStatePtr]))
 
 ;; =============== Factories ==================================================
 
@@ -38,6 +41,12 @@
    (symbol (format "%s%s%s" prefix type name)))
   ([type name]
    (lapacke "LAPACKE_" type name)))
+
+(defn math
+  ([prefix type name]
+   (symbol (format "%s%s%s" prefix type name)))
+  ([type name]
+   (math "v" type name)))
 
 (defmacro real-vector-blas* [name t ptr cast blas lapack blas-layout]
   `(extend-type ~name
@@ -107,6 +116,207 @@
          (dragan-says-ex "You cannot sort a vector with stride." {"stride" (stride x#)}))
        x#)))
 
+(defmacro vector-math
+  ([method ptr a y]
+   `(do
+      (check-stride ~a ~y)
+      (. mkl_rt ~method (dim ~a) (~ptr ~a) (~ptr ~y))
+      ~y))
+  ([method ptr a b y]
+   `(do
+      (check-stride ~a ~b ~y)
+      (. mkl_rt ~method (dim ~a) (~ptr ~a) (~ptr ~b) (~ptr ~y))
+      ~y)))
+
+(defmacro with-rng-check [x expr]
+  `(if (< 0 (dim ~x))
+     (if (= 1 (stride ~x))
+       (let [err# ~expr]
+         (if (= 0 err#)
+           ~x
+           (throw (ex-info "MKL error." {:error-code err#}))))
+       (dragan-says-ex "This engine cannot generate random entries in host vectors with stride. Sorry."
+                       {:v (info ~x)}))
+     ~x))
+
+(defn create-stream-ars5 ^mkl_rt$VSLStreamStatePtr [seed]
+  (let-release [stream (mkl_rt$VSLStreamStatePtr. (long-pointer 1))]
+    (with-mkl-check (mkl_rt/vslNewStream stream mkl_rt/VSL_BRNG_ARS5 seed)
+      stream)))
+
+(def ^:private default-rng-stream (create-stream-ars5 (generate-seed)))
+
+(defmacro real-vector-math* [name t ptr cast]
+  `(extend-type ~name
+     VectorMath
+     (sqr [this# a# y#]
+       (vector-math ~(math t 'Sqr) ~ptr a# y#))
+     (mul [this# a# b# y#]
+       (vector-math ~(math t 'Mul) ~ptr a# b# y#))
+     (div [this# a# b# y#]
+       (vector-math ~(math t 'Div) ~ptr a# b# y#))
+     (inv [this# a# y#]
+       (vector-math ~(math t 'Inv) ~ptr a# y#))
+     (abs [this# a# y#]
+       (vector-math ~(math t 'Abs) ~ptr a# y#))
+     (linear-frac [this# a# b# scalea# shifta# scaleb# shiftb# y#]
+       (check-stride a# b# y#)
+       (. mkl_rt ~(math t 'LinearFrac) (dim a#) (~ptr a#) (~ptr b#)
+          (~cast scalea#) (~cast shifta#) (~cast scaleb#) (~cast shiftb#) (~ptr y#))
+       y#)
+     (fmod [this# a# b# y#]
+       (vector-math ~(math t 'Fmod) ~ptr a# b# y#))
+     (frem [this# a# b# y#]
+       (vector-math  ~(math t 'Remainder) ~ptr a# b# y#))
+     (sqrt [this# a# y#]
+       (vector-math ~(math t 'Sqrt) ~ptr a# y#))
+     (inv-sqrt [this# a# y#]
+       (vector-math ~(math t 'InvSqrt) ~ptr a# y#))
+     (cbrt [this# a# y#]
+       (vector-math ~(math t 'Cbrt) ~ptr a# y#))
+     (inv-cbrt [this# a# y#]
+       (vector-math ~(math t 'InvCbrt) ~ptr a# y#))
+     (pow2o3 [this# a# y#]
+       (vector-math ~(math t 'Pow2o3) ~ptr a# y#))
+     (pow3o2 [this# a# y#]
+       (vector-math ~(math t 'Pow3o2) ~ptr a# y#))
+     (pow [this# a# b# y#]
+       (vector-math ~(math t 'Pow) ~ptr a# b# y#))
+     (powx [this# a# b# y#]
+       (check-stride a# y#)
+       (. mkl_rt ~(math t 'Powx) (dim a#) (~ptr a#) (~cast b#) (~ptr y#))
+       y#)
+     (hypot [this# a# b# y#]
+       (vector-math ~(math t 'Hypot) ~ptr a# b# y#))
+     (exp [this# a# y#]
+       (vector-math ~(math t 'Exp) ~ptr a# y#))
+     (exp2 [this# a# y#]
+       (vector-math ~(math t 'Exp2) ~ptr a# y#))
+     (exp10 [this# a# y#]
+       (vector-math ~(math t 'Exp10) ~ptr a# y#))
+     (expm1 [this# a# y#]
+       (vector-math ~(math t 'Expm1) ~ptr a# y#))
+     (log [this# a# y#]
+       (vector-math ~(math t 'Ln) ~ptr a# y#))
+     (log2 [this# a# y#]
+       (vector-math ~(math t 'Log2) ~ptr a# y#))
+     (log10 [this# a# y#]
+       (vector-math ~(math t 'Log10) ~ptr a# y#))
+     (log1p [this# a# y#]
+       (vector-math ~(math t 'Log1p) ~ptr a# y#))
+     (sin [this# a# y#]
+       (vector-math ~(math t 'Sin) ~ptr a# y#))
+     (cos [this# a# y#]
+       (vector-math ~(math t 'Cos) ~ptr a# y#))
+     (tan [this# a# y#]
+       (vector-math ~(math t 'Tan) ~ptr a# y#))
+     (sincos [this# a# y# z#]
+       (vector-math ~(math t 'SinCos) ~ptr a# y# z#))
+     (asin [this# a# y#]
+       (vector-math ~(math t 'Asin) ~ptr a# y#))
+     (acos [this# a# y#]
+       (vector-math ~(math t 'Acos) ~ptr a# y#))
+     (atan [this# a# y#]
+       (vector-math ~(math t 'Atan) ~ptr a# y#))
+     (atan2 [this# a# b# y#]
+       (vector-math ~(math t 'Atan2) ~ptr a# b# y#))
+     (sinh [this# a# y#]
+       (vector-math ~(math t 'Sinh) ~ptr a# y#))
+     (cosh [this# a# y#]
+       (vector-math ~(math t 'Cosh) ~ptr a# y#))
+     (tanh [this# a# y#]
+       (vector-math ~(math t 'Tanh) ~ptr a# y#))
+     (asinh [this# a# y#]
+       (vector-math ~(math t 'Asinh) ~ptr a# y#))
+     (acosh [this# a# y#]
+       (vector-math ~(math t 'Acosh) ~ptr a# y#))
+     (atanh [this# a# y#]
+       (vector-math ~(math t 'Atanh) ~ptr a# y#))
+     (erf [this# a# y#]
+       (vector-math ~(math t 'Erf) ~ptr a# y#))
+     (erfc [this# a# y#]
+       (vector-math ~(math t 'Erfc) ~ptr a# y#))
+     (erf-inv [this# a# y#]
+       (vector-math ~(math t 'ErfInv) ~ptr a# y#))
+     (erfc-inv [this# a# y#]
+       (vector-math ~(math t 'ErfcInv) ~ptr a# y#))
+     (cdf-norm [this# a# y#]
+       (vector-math ~(math t 'CdfNorm) ~ptr a# y#))
+     (cdf-norm-inv [this# a# y#]
+       (vector-math ~(math t 'CdfNormInv) ~ptr a# y#))
+     (gamma [this# a# y#]
+       (vector-math ~(math t 'TGamma) ~ptr a# y#))
+     (lgamma [this# a# y#]
+       (vector-math ~(math t 'LGamma) ~ptr a# y#))
+     (expint1 [this# a# y#]
+       (vector-math ~(math t 'ExpInt1) ~ptr a# y#))
+     (floor [this# a# y#]
+       (vector-math ~(math t 'Floor) ~ptr a# y#))
+     (fceil [this# a# y#]
+       (vector-math ~(math t 'Ceil) ~ptr a# y#))
+     (trunc [this# a# y#]
+       (vector-math ~(math t 'Trunc) ~ptr a# y#))
+     (round [this# a# y#]
+       (vector-math ~(math t 'Round) ~ptr a# y#))
+     (modf [this# a# y# z#]
+       (vector-math ~(math t 'Modf) ~ptr a# y# z#))
+     (frac [this# a# y#]
+       (vector-math ~(math t 'Frac) ~ptr a# y#))
+     (fmin [this# a# b# y#]
+       (vector-math ~(math t 'Fmin) ~ptr a# b# y#))
+     (fmax [this# a# b# y#]
+       (vector-math ~(math t 'Fmax) ~ptr a# b# y#))
+     (copy-sign [this# a# b# y#]
+       (vector-math ~(math t 'CopySign) ~ptr a# b# y#))
+     (sigmoid [this# a# y#]
+       (sigmoid-over-tanh this# a# y#))
+     (ramp [this# a# y#]
+       (vector-ramp this# a# y#))
+     (relu [this# alpha# a# y#]
+       (vector-relu this# alpha# a# y#))
+     (elu [this# alpha# a# y#]
+       (vector-elu this# alpha# a# y#))))
+
+(defn float-uniform [^mkl_rt$VSLStreamStatePtr stream ^double lower ^double upper x]
+  (with-rng-check x
+    (mkl_rt/vsRngUniform mkl_rt/VSL_RNG_METHOD_UNIFORM_STD stream (dim x) (float-ptr x) lower upper)))
+
+(defn float-gaussian [^mkl_rt$VSLStreamStatePtr stream ^double mu ^double sigma x]
+  (with-rng-check x
+    (mkl_rt/vsRngGaussian mkl_rt/VSL_RNG_METHOD_GAUSSIAN_BOXMULLER2 stream (dim x) (float-ptr x) mu sigma)))
+
+(defn double-uniform [^mkl_rt$VSLStreamStatePtr stream ^double lower ^double upper x]
+  (with-rng-check x
+    (mkl_rt/vdRngUniform mkl_rt/VSL_RNG_METHOD_UNIFORM_STD stream (dim x) (double-ptr x) lower upper)))
+
+(defn double-gaussian [^mkl_rt$VSLStreamStatePtr stream ^double mu ^double sigma x]
+  (with-rng-check x
+    (mkl_rt/vdRngGaussian mkl_rt/VSL_RNG_METHOD_GAUSSIAN_BOXMULLER2 stream (dim x) (double-ptr x) mu sigma)))
+
+(deftype FloatVectorEngine [])
+(real-vector-blas* FloatVectorEngine "s" float-ptr float mkl_rt mkl_rt mkl-blas-layout)
+(real-vector-math* FloatVectorEngine "s" float-ptr float)
+
+(extend-type FloatVectorEngine
+  RandomNumberGenerator
+  (rand-uniform [_ rng-stream lower upper x]
+    (float-uniform (or rng-stream default-rng-stream) lower upper x))
+  (rand-normal [this rng-stream mu sigma x]
+    (with-rng-check x
+      (float-gaussian (or rng-stream default-rng-stream) mu sigma x))))
+
+(deftype DoubleVectorEngine [])
+(real-vector-blas* DoubleVectorEngine "d" double-ptr double mkl_rt mkl_rt mkl-blas-layout)
+(real-vector-math* DoubleVectorEngine "d" double-ptr double)
+
+(extend-type DoubleVectorEngine
+  RandomNumberGenerator
+  (rand-uniform [_ rng-stream lower upper x]
+    (double-uniform (or rng-stream default-rng-stream) lower upper x))
+  (rand-normal [this rng-stream mu sigma x]
+    (with-rng-check x
+      (double-gaussian (or rng-stream default-rng-stream) mu sigma x))))
+
 (deftype MKLRealFactory [index-fact ^DataAccessor da
                          vector-eng]
   DataAccessorProvider
@@ -124,7 +334,7 @@
     (compatible? da o))
   RngStreamFactory
   (create-rng-state [_ seed]
-    #_(create-stream-ars5 seed));;TODO
+    (create-stream-ars5 seed))
   Factory
   (create-vector [this n init]
     (let-release [res (real-block-vector this n)]
@@ -137,12 +347,6 @@
 
 (def float-accessor (->FloatPointerAccessor malloc!))
 (def double-accessor (->DoublePointerAccessor malloc!))
-
-(deftype FloatVectorEngine [])
-(real-vector-blas* FloatVectorEngine "s" float-ptr float mkl_rt mkl_rt mkl-blas-layout)
-
-(deftype DoubleVectorEngine [])
-(real-vector-blas* DoubleVectorEngine "d" double-ptr double mkl_rt mkl_rt mkl-blas-layout)
 
 (def mkl-float
   (->MKLRealFactory mkl-int float-accessor
